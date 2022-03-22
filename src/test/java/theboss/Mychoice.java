01a22581bad8646144ab7675de0f00f51d9e94af
Your Name <you@example.com>
2022-03-22 14:37:02 +0530

    This my projects

---
+++ src/test/java/theboss/Mychoice.java
@@ -0,0 +1,58 @@
+package theboss;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Mychoice {
+	public static 	WebDriver driver;
+	@BeforeTest
+	public void init()   {
+		System.setProperty("webdriver.chrome.driver","F:\\javaselenium\\LiveTech Selenium Software\\Browsers\\New 95 chrome\\chromedriver.exe");
+		 driver=new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+			driver.get("https://www.mychoicesmyplan.org/");
+
+		// driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+	//	driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+	}
+
+	
+	@Test
+	public void login() throws Throwable {
+		driver.findElement(By.xpath("//*[@id=\"tophead\"]/div/div/div/div[2]/a")).click();
+		//Thread.sleep(3000);
+
+		driver.findElement(By.id("erf_username")).sendKeys("pradeep.p@mavensoft.com");
+	//	Thread.sleep(3000);
+		driver.findElement(By.id("erf_password")).sendKeys("123456");
+		driver.findElement(By.xpath("//*[@id=\"post-1945\"]/div/div/div[2]/div/div[1]/form[1]/div[7]/div/button")).click();
+	}
+
+	
+	
+
+	@Test
+	public void logout() {
+	driver.findElement(By.xpath("//*[@id=\"tophead\"]/div/div/div/div[2]/div/a")).click();
+
+		
+	}
+
+	@AfterTest
+	public void quit() {
+	
+	driver.close();
+	
+	}
+}
+
